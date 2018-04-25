5cb116e743ff6fc0dafbada115e3f61648a8560a
Heo Soongang <paperworker.hsg@gmail.com>
2018-04-25 09:28:27 +0900

    first commit

---
+++ content/my-first-post.md
@@ -0,0 +1,10 @@
+Title: My first post
+Date: 2018-04-25 09:15
+Modified: 2018-04-25 09:15
+Category: misc
+Tags: first, misc
+Slug: My-first-post
+Authors: Paperworker
+Summary: my first post
+
+This is the *first post* from my Pelican blog.
---
+++ pelicanconf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = '종이접는 사회사업가'
+SITENAME = '종이접는 사회사업가'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Seoul'
+
+DEFAULT_LANG = 'ko'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = 'feeds/%s.atom.xml'
+AUTHOR_FEED_RSS = 'feeds/%s.rss.xml'
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
